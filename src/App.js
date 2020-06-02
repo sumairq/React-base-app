fea6ba234cef4640a7e51a9bcb5ab24876c72eda
Sumair <sumairqaisar992@gmail.com>
2020-06-02 16:02:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sumair's<code>first React App </code>.
+          Sumair's<code> first React App </code>.
         </p>
         <a
           className="App-link"
